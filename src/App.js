5564197b8512f117e45c5d78674b52a688f16296
Kareem Taleb <kareemostyle@gmail.com>
2024-11-16 13:37:17 -0500

    Updated CSS and added scoreboard

--- src/App.js
+++ src/App.js
@@ -12,12 +12,24 @@ function App() {
         </div>
         <div className="guess">
           <ol className="answer-list">
-            <li id="first-answer">1998</li>
-            <li id="second-answer">2004</li>
-            <li id="third-answer">2003</li>
-            <li id="fourth-answer">2005</li>
+            <li className="answer">1998</li>
+            <li className="answer">2004</li>
+            <li className="answer">2003</li>
+            <li className="answer">2005</li>
           </ol>
         </div>
+      </div>
+      <div className="score-container" id="scoreboard">
+        <div className="player-score">
+          <h2 className="score-text">Player</h2>
+          <div className="score">
+            <p className="points" id="player-points">
+              0
+            </p>
+            <p className="points-shadow">000</p>
+          </div>
+        </div>
+        <div className="possible-score"></div>
       </div>
     </div>
   );
